ab5d464a3fd38b742e8cb925e471990a593365d2
Valmir Almeida <almeidavalmir76@gmail.com>
2023-05-27 01:12:52 -0300

    Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Image from 'next/image'
+import styles from "./styles/home.module.scss";
 
 export default function Home() {
   return (
-    <main>
-     Main
+    <main className={styles.main}>
+      <section id="home" className={styles.home}>
+        <div className={styles.text}>
+          <h1>Higienização interna de veículos, lavagem e demais serviços.</h1>
+          <p>
+            Oferecemos uma variedade de serviços para veículos, como a aplicação
+            de Insulfilm para vidros automotivos e residenciais, polimento e
+            cristalização de pintura para restaurar o brilho e proteger contra
+            danos, além da higienização interna completa, garantindo um ambiente
+            limpo e confortável. Nosso objetivo é proporcionar cuidados
+            automotivos de qualidade, mantendo seu veículo com uma aparência
+            impecável e protegido.
+          </p>
+          <a href="#about">Saíba Mais</a>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
